4bcd5c3057acec08dd325f5563537812165604c8
Abigirl Muchineripi <muchineripiabby@gmail.com>
2025-03-26 02:31:20 +0200

    changes added
    added tablet view

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let  i = 0;
+const target = 10000;
+const increment = 500;
+const h1 = document.getElementById('main-card__section1-title');
+
+function updateCount() {
+    if (i <= target) {
+        h1.textContent = `${i}+ satisfied customers`;
+        i += increment;
+        setTimeout(updateCount, 100); 
+    }
+}
+
+window.onload = function() {
+        updateCount();    
+};
